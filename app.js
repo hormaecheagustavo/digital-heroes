c2a358c3b0d535612535c94ef54e2b806ffa170b
donmariodistribuciones@gmail.com <patriciaortt@MacBook-Air-de-Patricia.local>
2020-09-08 22:16:11 -0300

    arregle una parte pero voy a tener q ver nuevamente la parte de res.sendfile.

--- app.js
+++ app.js
@@ -1,15 +1,14 @@
 const express = require("express");
 const app = express();
 const path = require("path");
-const { get } = require("http");
-const heroes = require("heroes.json")
-app.get("/", funtion (req,res){
-  res.send("​Ni Superman, IronMan o La Mujer Maravilla son tan importantes cómo las y los Heroes de carne yhueso que encontrarás en este sitio. Esperamos que ellas y ellos te sirvan comoinspiración para poder cumplir tus objetivos. Recuerda: ¡nunca pares de creer enti!.")
+let heroes = path.resolve("heroes.json");
+app.get("/", function (req, res) {
+  res.send(
+    "​Ni Superman, IronMan o La Mujer Maravilla son tan importantes cómo las y los Heroes de carne yhueso que encontrarás en este sitio. Esperamos que ellas y ellos te sirvan comoinspiración para poder cumplir tus objetivos. Recuerda: ¡nunca pares de creer enti!."
+  );
 });
-app.get ("/heroes", function(req,res){
-  
-  res.send("heroes")
+app.get("/heroes", function (req, res) {
+  res.sendfile("heroes");
 });
 
-
-get.listen(3000);
+app.listen(3030, () => console.log("Esto funciona"));
